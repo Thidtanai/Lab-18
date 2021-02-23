c75189afa9c49e94e2292b01e457cffee5f6ec47
Thidsloth <76035488+Thidtanai@users.noreply.github.com>
2021-02-23 16:41:52 +0700

    coomplete

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *in1, Rect *in2){
+    if(in1->x+in1->w < in2->x)return 0;
+    float l1 = fmin(in1->x + in1->w,in2->x + in2->w) - fmax(in1->x,in2->x);
+    float l2 = fmin(in1->y,in2->y) - fmax(in1->y-in1->h,in2->y-in2->h);
+    return l1 * l2;
 }
